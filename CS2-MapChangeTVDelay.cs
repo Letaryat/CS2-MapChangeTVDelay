66ede93f2100ba940b75a624f3bc79b57e21ec70
Letaryat <69041891+Letaryat@users.noreply.github.com>
2024-12-28 21:16:33 +0100

    0.0.1

---
+++ CS2-MapChangeTVDelay.cs
@@ -0,0 +1,37 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using static CounterStrikeSharp.API.Core.Listeners;
+using Microsoft.Extensions.Logging;
+
+namespace CS2MapChangeStopTV;
+public class CS2MapChangeStopTV : BasePlugin
+{
+    public override string ModuleName => "CS2-MapChangeStopTV";
+    public override string ModuleVersion => "0.0.1";
+    public override string ModuleAuthor => "Letaryat";
+    public override string ModuleDescription => "Delays map change and stops tv record";
+    public override void Load(bool hotReload)
+    {
+        Logger.LogInformation("CS2-MapChangeStopTV - Loaded");
+        AddCommandListener("changelevel", ListenerChangeLevel, HookMode.Pre);
+        RegisterListener<OnMapStart>((mapname) =>{
+            var fileName = $"auto-{DateTime.Now:yyyyMMdd-HHmm}-{mapname}-CS2_____Arena_1v1_____Pierdolnik.eu___1shot1kill.pl";
+            Server.NextWorldUpdate(() => Server.ExecuteCommand($"tv_record \"replays/{fileName}.dem\""));
+            Logger.LogInformation($"Started recording demo: {fileName}");
+        });
+    }
+    public override void Unload(bool hotReload) 
+    {
+        Logger.LogInformation("CS2-MapChangeStopTV - Unloaded");
+    }
+    public HookResult ListenerChangeLevel(CCSPlayerController? player, CommandInfo info)
+    {
+        Server.ExecuteCommand("tv_stoprecord");
+        AddTimer(1.0f, () =>{
+            Logger.LogInformation($"Stopped TV Record. Delay before changing map.");
+        });
+        return HookResult.Continue;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CS2-MapChangeTVDelay
- 
+Changing map could cause Counter-Strike 2 servers to crash if they are using GOTV. This plugin should fix that. <br>
+Thanks to [K4-GOTV-Discord](https://github.com/KitsuneLab-Development/K4-GOTV-Discord/tree/dev) for a way to listen changelevel and also for general idea how to fix this problem.
+<br><img src="https://i.imgur.com/TQP4lYn.gif" height="200px">
+
+## How does it work?
+Plugin listens if changelevel was executed, if so it stops recording the map and then changes the level. When next map starts it start to record a new demo.
+
+### [🚨] Plugin might be poorly written and have some issues. I have no idea what I am doing.
