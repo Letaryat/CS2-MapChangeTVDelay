4df8974ff25aaf6526c6764399cc6b262a034fac
Letaryat <69041891+Letaryat@users.noreply.github.com>
2024-12-29 11:11:59 +0100

    0.0.3
    Added config

--- CS2-MapChangeTVDelay.cs
+++ CS2-MapChangeTVDelay.cs
@@ -3,51 +3,84 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Commands;
 using static CounterStrikeSharp.API.Core.Listeners;
 using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
+using CounterStrikeSharp.API.Modules.Cvars;
 
 namespace CS2MapChangeStopTV;
-public class CS2MapChangeStopTV : BasePlugin
+
+public class MapChangeStopTV : BasePluginConfig
+{
+    [JsonPropertyName("ReplayName")] public string ReplayName { get; set; } = "demo-{mapname}-{servername}-{date}-{time}-{timedate}";
+    [JsonPropertyName("Debug")] public bool Debug { get; set; } = true;
+}
+public class CS2MapChangeStopTV : BasePlugin, IPluginConfig<MapChangeStopTV>
 {
     public override string ModuleName => "CS2-MapChangeStopTV";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.3";
     public override string ModuleAuthor => "Letaryat";
     public override string ModuleDescription => "Delays map change and stops tv record";
     public string? fileName;
+    public MapChangeStopTV Config { get; set; }
+    public void OnConfigParsed(MapChangeStopTV config)
+    {
+        Config = config;
+    }
+
     public override void Load(bool hotReload)
     {
-        Logger.LogInformation("CS2-MapChangeStopTV - Loaded");
+        LogDebug("CS2-MapChangeStopTV - Loaded");
+        //Listeners:
         AddCommandListener("changelevel", ListenerChangeLevel, HookMode.Pre);
         AddCommandListener("map", ListenerChangeLevel, HookMode.Pre);
-		AddCommandListener("host_workshop_map", ListenerChangeLevel, HookMode.Pre);
-		AddCommandListener("ds_workshop_changelevel", ListenerChangeLevel, HookMode.Pre);
+        AddCommandListener("host_workshop_map", ListenerChangeLevel, HookMode.Pre);
+        AddCommandListener("ds_workshop_changelevel", ListenerChangeLevel, HookMode.Pre);
+
         RegisterListener<OnMapStart>((mapname) => {
             fileName = null;
-            Logger.LogInformation($"Filename = null");
+            LogDebug($"New map start ({mapname}). Filename = null");
         });
         RegisterEventHandler<EventCsWinPanelMatch>((e, i) => {
             Server.ExecuteCommand("tv_stoprecord");
-            Logger.LogInformation($"Stopped TV Record by end panel.");
+            LogDebug("Recording stopped because of EventCsWinPanelMatch");
             return HookResult.Continue;
         });
         RegisterEventHandler<EventRoundStart>((e, i) =>
         {
-            var mapname = Server.MapName;
             if(fileName != null) {return HookResult.Continue;}
-            fileName = $"auto-{DateTime.Now:yyyyMMdd-HHmm}-{mapname}-CS2_____Arena_1v1_____Pierdolnik.eu___1shot1kill.pl";
+            fileName = Config.ReplayName;
+            var placeholdersReplacement = new Dictionary<string, string>
+            {
+                {"{mapname}", Server.MapName},
+                {"{servername}", ConVar.Find("hostname")?.StringValue ?? "Unknown"},
+                {"{date}", DateTime.Now.ToString("yyyy-MM-dd")},
+                {"{time}", DateTime.Now.ToString("HH-mm-ss")},
+                {"{timedate}", DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")},
+                {"{1s1kformat}", $"auto-{DateTime.Now:yyyyMMdd-HHmm}-{Server.MapName}"}
+            };
+            foreach(var placeholder in placeholdersReplacement)
+            {
+                fileName = fileName.Replace(placeholder.Key, placeholder.Value);
+            }
             Server.NextWorldUpdate(() => Server.ExecuteCommand($"tv_record \"replays/{fileName}.dem\""));
-            Logger.LogInformation($"Started recording demo: {fileName}");
+            LogDebug($"Recording demo: {fileName}");
             return HookResult.Continue;
         });
     }
     public override void Unload(bool hotReload) 
     {
-        Logger.LogInformation("CS2-MapChangeStopTV - Unloaded");
+        LogDebug("CS2-MapChangeStopTV - Unloaded");
         Server.ExecuteCommand("tv_stoprecord");
     }
     public HookResult ListenerChangeLevel(CCSPlayerController? player, CommandInfo info)
     {
         Server.ExecuteCommand("tv_stoprecord");
-        Logger.LogInformation($"Stopped TV Record. Delay before changing map.");
+        LogDebug("Stopped TV Record before changing map.");
         return HookResult.Continue;
+    }
+    public void LogDebug(string message)
+    {
+        if(Config.Debug == false) {return;}
+        Logger.LogInformation($"{DateTime.Now} | {message}");
     }
 
 }
--- README.md
+++ README.md
@@ -3,7 +3,42 @@ Changing map could cause Counter-Strike 2 servers to crash if they are using GOT
 Thanks to [K4-GOTV-Discord](https://github.com/KitsuneLab-Development/K4-GOTV-Discord/tree/dev) for a way to listen changelevel and also for general idea how to fix this problem.
 <br><img src="https://i.imgur.com/TQP4lYn.gif" height="200px">
 
-## How does it work?
+## [📌] Setup
+- Download latest release,
+- Drag files to /plugins/
+- Restart your server,
+- Config file should be created in configs/plugins/CS2-MapChangeTVDelay,
+- Edit to your liking,
+
+```
+{
+  "ReplayName": "demo-{mapname}-{servername}-{date}-{time}-{timedate}", - You can use placeholders listed below (string)
+  "Debug": true, - if it should log everything that plugin does (bool)
+  "ConfigVersion": 1
+}
+```
+
+## [📌] Recommended GOTV settings:
+```
+tv_enable 1
+tv_name "Your GOTV Name"
+tv_relayvoice 1
+tv_delaymapchange 1
+tv_delay 0
+tv_autorecord 0
+```
+
+## [📜] Placeholders:
+| Placeholder  | Description |
+| ------------- | ------------- |
+| {mapname}  | Name of played map  |
+| {servername}  | Server name (if it cannot fetch it, it returns "Unknown")  |
+| {date}  | Returns date in format yyyy-mm-dd  |
+| {time}  | Returns time in format HH-mm-ss  |
+| {timedate}  | Returns time and date in format yyyy-MM-dd_HH-mm-ss  |
+| {1s1kformat}  | Demo format that is used by 1s1k.pl to store demos. Returns auto-**Date yyyyMMdd-HHmm**-ServerName |
+
+### [🛠️] How does it work?
 Plugin listens if changelevel was executed, if so it stops recording the map and then changes the level. When next map starts it start to record a new demo.
 
-### [🚨] Plugin might be poorly written and have some issues. I have no idea what I am doing.
+### [🚨] Plugin might be poorly written and have some issues. I have no idea what I am doing. Even so, when tested it works as intended.
